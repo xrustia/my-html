84a60c8222b09129b9a78b0c0e371ad5e922c76a
xrustia <xrustunalipkevuch1231@gmail.com>
2021-11-16 16:12:35 +0200

    gh repo clone xrustia/my-html

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const clockContainer = document.querySelector('.clock');
+setInterval(() => clockContainer.innerText = new Date().toLocaleTimeString(), 1000);
+
+
+const countDownContainer = document.querySelector('.countdown');
+const startTime = new Date();
+function showCountDown() {
+    const limitSecs = 10;
+    const currentTime = new Date();
+    const intervalSecs = Math.round((currentTime - startTime) / 1000);
+    const leftSecs = limitSecs - intervalSecs;
+    countDownContainer.innerText = leftSecs;
+    if (leftSecs <= 0) {
+        document.body.innerHTML = 'НА ВАШОМУ РАХУНКУ НЕДОСТАТНЬО КОШТІВ ;)';
+    }
+
+}
+setInterval(showCountDown, 1000);
